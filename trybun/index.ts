d66f8c735c4342ea5b581a352d2b0bcfe7412931
aguttero <aguttero@gmail.com>
2026-02-05 18:25:11 -0300

    add bun project v0

---
+++ trybun/index.ts
@@ -0,0 +1,17 @@
+console.log("Hello via Bun!");
+console.log("My very first bun project!!");
+
+// function name(): type {
+//     ...
+// }
+
+function sum(a: number,b: number): number{
+    return a+b;
+}
+
+
+var ans = sum(7,6);
+
+console.log(ans);
+ans ++;
+console.log(ans);
