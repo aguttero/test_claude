a0fe04c23d918b3222623af2013518c4592d6fe9
aguttero <aguttero@gmail.com>
2026-02-27 17:58:10 -0300

    0227pm01 elysia get and post

--- bun-first/bun-rest-api/src/index.ts
+++ bun-first/bun-rest-api/src/index.ts
@@ -2,7 +2,42 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Bun dev! This is Elysia \n I'm gonna build a RESTFUL API!!")
 .get('/post/:id', ({params: {id}}) => {return {id: id, title: 'Learn Bun'}})
-// .post('/post',({body}))
+.post('/context', (context) => {return context})
+.post('/bodyfull', (body) => {return body})
+.post('/bodyclean', (body) => {return body.body})
+.post('/bodyonly', ({body}) => {return body})
+.post('/bodyset', ({body, set}) => {
+  set.status = 201
+  return body
+})
+.get('/track/*', () => {return {text:'title', title: 'wildcard answer'}})
+// return JSON Array
+.get('/jsonarray1', () => {
+  return new Response(JSON.stringify({
+    "tracks": [
+      'Dancing Feat',
+      'Sam I',
+      'Animals'
+    ]
+  })
+  ,{
+     headers: {
+      'Content-Type':'application/json'
+     }
+    }
+ )
+})
+.get('/jsonarray2', () => {
+  return {
+    "tracks": [
+      'Dancing Feat',
+      'Sam I',
+      'Animals',
+      'New Song'
+    ]
+  }
+  })
+//.post('/post',({body}))
 .listen(3000);
 
 console.log(
